2e987a5e8b77f8baa84b2748c1ae62da77f74a9b
rumihsieh <87258074+rumihsieh@users.noreply.github.com>
2021-07-14 14:58:02 +0800

    Update and rename 123.py to 猜數字小遊戲.py

--- 猜數字小遊戲.py
+++ 猜數字小遊戲.py
@@ -1,7 +1,7 @@
-import random
-
 r = random. randint(1,100)
+count = 0
 while True:
+	count = count + 1
 	num = input('請猜數字：')
 	num = int(num)
 	if num == r:
@@ -11,3 +11,5 @@ while True:
 		print('比他小一點')
 	elif num < r:
 		print('比他大一點')
+	print('這是你猜的第',count, '次')
+
